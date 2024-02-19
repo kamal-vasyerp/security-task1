e54a9f06e3834d59dbba7d98e01b8cbd0bf8fd49
Kamal Jain <kamaljain@vasyerp.com>
2024-02-19 09:08:32 +0530

    task started

---
+++ src/main/java/com/practise/securitytask/SecurityTask1Application.java
@@ -0,0 +1,13 @@
+package com.practise.securitytask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityTask1Application.class, args);
+	}
+
+}
